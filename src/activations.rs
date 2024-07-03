472b4b9c7a29c94052f8f1702978a88af1e20d8e
towerpark <24645162+towerpark@users.noreply.github.com>
2024-07-03 21:39:07 +0900

    Encapsulate sigmoid operations into a struct
    Add the Activation trait and implement it on the Sigmoid struct so that
    we can easily try activation functions other than sigmoid later.

---
+++ src/activations.rs
@@ -0,0 +1,23 @@
+use super::common::A2;
+
+
+pub trait Activation {
+    fn call(z: &A2) -> A2;
+
+    fn prime(z: &A2) -> A2;
+}
+
+
+pub struct Sigmoid;
+
+impl Activation for Sigmoid {
+    fn call(z: &A2) -> A2 {
+        z.mapv(|e| 1.0f32 / (1.0f32 + std::f32::consts::E.powf(-e)))
+    }
+
+    fn prime(z: &A2) -> A2 {
+        let mut s = Self::call(&z);
+        s.map_inplace(|e| *e *= 1.0_f32 - *e);
+        s
+    }
+}
